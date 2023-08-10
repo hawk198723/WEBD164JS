86f3cb874724417269fe2e67d7936d5f32db99a6
hawk198723 <hawk198723@gmail.com>
2023-08-09 19:03:16 -0700

    add comments

--- final/jsjqfinal.js
+++ final/jsjqfinal.js
@@ -1,35 +1,44 @@
+// Defining the main DOM elements
 const container = document.querySelector(".container");
 const search = document.querySelector(".search-box button");
 const weatherBox = document.querySelector(".weather-box");
 const weatherDetails = document.querySelector(".weather-details");
 const error404 = document.querySelector(".not-found");
 
+// Add a click event to the search button
 search.addEventListener("click", () => {
+  // Retrieve city name from the input box
   const city = document.querySelector(".search-box input").value;
+  // API key
   const APIKey = "f6b0084c5da865d7ded4f360f784b540";
+  // Make sure the city name is not empty
   console.log("Button clicked! Searching for city:", city);
   if (city === "") {
     return;
   }
-
+  // Use fetch to call the OpenWeatherMap API to get weather data for the specified city
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
   )
     .then((response) => {
+      // If the response is not okay, throw an error
       if (!response.ok) {
         return response.json().then((json) => Promise.reject(json));
       }
       return response.json();
     })
     .then((json) => {
-      console.log("API response:", json);
+      // for testing API data in the console
+      // console.log("API response:", json);
 
-      console.log("main:", json.main);
-      console.log("weather:", json.weather);
-      console.log("humidity:", json.main.humidity);
-      console.log("speed:", json.wind.speed);
+      // console.log("main:", json.main);
+      // console.log("weather:", json.weather);
+      // console.log("humidity:", json.main.humidity);
+      // console.log("speed:", json.wind.speed);
 
+      // Check the API response
       if (json.cod === 404 || json.cod === "404") {
+        // Display error message
         container.style.height = "400px";
         weatherBox.style.display = "none";
         weatherDetails.style.display = "none";
@@ -37,18 +46,21 @@ search.addEventListener("click", () => {
         error404.classList.add("fadeIn");
         return;
       } else if (json.cod === 200 || json.cod === "200") {
+        // Display the weather data
         weatherBox.style.display = "";
         weatherDetails.style.display = "";
         error404.style.display = "none";
         error404.classList.remove("fadeIn");
       } else {
+        // Handle unknown API response
         console.error("Unknown API response:", json);
         return;
       }
 
-      error404.style.display = "none";
-      error404.classList.remove("fadeIn");
+      // error404.style.display = "none";
+      // error404.classList.remove("fadeIn");
 
+      // Update the UI elements to show weather data
       const image = document.querySelector(".weather-box img");
       const temperature = document.querySelector(".weather-box .temperature");
       const description = document.querySelector(".weather-box .description");
@@ -57,6 +69,7 @@ search.addEventListener("click", () => {
       );
       const wind = document.querySelector(".weather-details .wind span");
 
+      // Choose an image based on the weather type
       switch (json.weather[0].main) {
         case "Clear":
           image.src = "images/clear.png";
@@ -77,6 +90,7 @@ search.addEventListener("click", () => {
           image.src = "";
       }
 
+      // Update the UI with the weather data
       temperature.innerHTML = `${parseInt(json.main.temp)}<span> °C</span>`;
       description.innerHTML = `${json.weather[0].description}`;
       humidity.innerHTML = `${json.main.humidity}%`;
@@ -89,6 +103,7 @@ search.addEventListener("click", () => {
       container.style.height = "590px";
     })
     .catch((error) => {
+      // Handle the error from the API call
       console.error("Error fetching data: ", error);
       if (error.cod === 404 || error.cod === "404") {
         container.style.height = "400px";
